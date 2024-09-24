0a5bafba4e02a09c5ae3e178683840370e314c97
Jesan <akterjesan@gmail.com>
2024-09-24 07:21:53 +0100

    2.4

--- backtesting_steamlit.py
+++ backtesting_steamlit.py
@@ -22,9 +22,7 @@ def fetch_ohlcv_data(file_path):
     # Floor the index to minute precision (ignoring seconds)
     ohlcv_data.index = ohlcv_data.index.floor('min')
 
-    f_ohlcv_data = ohlcv_data[(ohlcv_data.index >= start_date) & (ohlcv_data.index <= end_date)]
-
-    return f_ohlcv_data
+    return ohlcv_data
 
 # Main logic to process liquidation data and check conditions
 def backtest_all_trades(start_date, end_date, timeframe, starting_equity):
